d759b4fc5184eb2f431d84692f91e00f8874d4c8
MSebb23 <matteosabbatini23@gmail.com>
2021-05-07 16:44:10 +0200

    Created the structure

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <title>Laravel-Molisana</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+</head>
+<body>
+
+    <header>
+        <img src=" {{ asset('images/marchio-sito-test.png') }} " alt="Logo della pasta molisana">
+    </header>
+</body>
+</html>
